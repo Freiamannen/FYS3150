7e06531e21cdb741ce45ee212a40179ad5724072
Freiamannen <www.fredrik.com@gmail.com>
2022-09-13 02:23:35 +0200

    Made progress

--- Projects/project_1/problem_7/header.hpp
+++ Projects/project_1/problem_7/header.hpp
@@ -2,10 +2,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 // Forward declaration of relevant functions
-std::vector<double> calc_b();
+std::vector<double> calc_b_t();
 std::vector<double> calc_g();
+std::vector<double> calc_g_t();
 std::vector<double> calc_v();
 
 // Defining matrix A
@@ -13,6 +15,13 @@ std::vector<double> calc_v();
 //    of the different diagonals.
 //
 int n = 1000;
+double x_min = 0.0;
+double x:max = 1.0;
+double h = (x_max - xmin) / n;
+// std::vector<double> x(n);
+// for (int i = 0; i < xSteps; i++) {
+//  x[i] = xStartingPoint + i * xStepSize;
+// }
 std::vector<int> a_diag(n, -1);
 std::vector<int> b_diag(n, 2);
 std::vector<int> c_diag(n, -1);
--- Projects/project_1/problem_7/main.cpp
+++ Projects/project_1/problem_7/main.cpp
@@ -6,3 +6,56 @@ int main() {
 
   return 0;
 }
+
+//
+// Defining the functions
+//
+
+std::vector<double> calc_b_t(std::vector<int> a, std::vector<int> b, std::vector<int> c, int n){
+  //
+  // Calculating b-tilde
+  // Arguments: a, b, c, n
+  std::vector<double> b_tilde(n);
+
+  // Formulars from problem 6.
+  for (int i = 1; i < n; i++) {
+    if (i == 1) {
+      b_tilde[i] = b[i];
+    }
+    else {
+      b_tilde[i] = b[i] - ( ( a[i] / b_tilde[i-1] ) * c[i-1]);
+    }
+  }
+
+  return b_tilde
+}
+
+std::vector<double> calc_g() {
+  //
+  // Calculating g
+  // Arguments: x, n, h
+  int g = 1
+
+  return g
+}
+
+
+std::vector<double> calc_g_t(std::vector<int> a, std::vector<int> b_t, int n, double h) {
+  //
+  // Calculating g-tilde
+  // Arguments: a, b-tilde, n, h
+  std::vector<double> g_tilde(n);
+  std::double f_1 = 100 * exp( (-10) * h );
+
+  // Formulars from problem 6.
+  for (int i = 1; i < n; i++) {
+    if (i == 1) {
+      g_tilde[i] = (h**2) * f_1;
+    }
+    else {
+      g_tilde[i] = b[i] - ( ( a[i] / b_tilde[i-1] ) * c[i-1]);
+    }
+  }
+
+  return g_tilde
+}
