4b718d78f7bdfdf9d8aa66051c5374fa77481fde
Freiamannen <www.fredrik.com@gmail.com>
2022-09-13 12:52:26 +0200

    Made further progress and added the solutions to problem 1, 3 and 4

--- Projects/project_1/problem_7/header.hpp
+++ Projects/project_1/problem_7/header.hpp
@@ -5,23 +5,8 @@
 #include <cmath>
 
 // Forward declaration of relevant functions
+std::vector<double> f();
 std::vector<double> calc_b_t();
 std::vector<double> calc_g();
 std::vector<double> calc_g_t();
 std::vector<double> calc_v();
-
-// Defining matrix A
-// -> Defining matrix A by containers with the values
-//    of the different diagonals.
-//
-int n = 1000;
-double x_min = 0.0;
-double x:max = 1.0;
-double h = (x_max - xmin) / n;
-// std::vector<double> x(n);
-// for (int i = 0; i < xSteps; i++) {
-//  x[i] = xStartingPoint + i * xStepSize;
-// }
-std::vector<int> a_diag(n, -1);
-std::vector<int> b_diag(n, 2);
-std::vector<int> c_diag(n, -1);
--- Projects/project_1/problem_7/main.cpp
+++ Projects/project_1/problem_7/main.cpp
@@ -4,6 +4,39 @@
 // Defining the main function
 int main() {
 
+  // Defining relevant variables
+  int n = 1000;
+  double x_min = 0.0;
+  double x:max = 1.0;
+  double h = (x_max - xmin) / n;
+
+  // Defining relevant containers
+  std::vector<double> x(n);
+  std::vector<double> b_tilde(n);
+  std::vector<double> g_tilde(n);
+
+  // Defining matrix A
+  // -> Defining matrix A by containers with the values
+  //    of the different diagonals.
+  //
+  std::vector<int> a_diag(n, -1);
+  std::vector<int> b_diag(n, 2);
+  std::vector<int> c_diag(n, -1);
+
+  // Initial values
+  x[0] = h;
+  b_tilde[0] = b_diag[0];
+  g_tilde[0] = f(x[0]) * h*h;
+
+  // Forward substitution
+  //
+  //
+
+
+  // Backward substitution
+  //
+  //
+
   return 0;
 }
 
@@ -11,11 +44,20 @@ int main() {
 // Defining the functions
 //
 
+std::vector<double> f(double x){
+  //
+  // Calculating a f(x)-value
+  // Arguments: x (a single value)
+
+  return 100 * exp( (-10) * x );
+}
+
 std::vector<double> calc_b_t(std::vector<int> a, std::vector<int> b, std::vector<int> c, int n){
   //
   // Calculating b-tilde
   // Arguments: a, b, c, n
-  std::vector<double> b_tilde(n);
+
+  //std::vector<double> b_tilde(n);
 
   // Formulars from problem 6.
   for (int i = 1; i < n; i++) {
@@ -27,25 +69,25 @@ std::vector<double> calc_b_t(std::vector<int> a, std::vector<int> b, std::vector
     }
   }
 
-  return b_tilde
+  return b_tilde;
 }
 
 std::vector<double> calc_g() {
   //
   // Calculating g
   // Arguments: x, n, h
-  int g = 1
 
-  return g
+
+  return g;
 }
-
 
 std::vector<double> calc_g_t(std::vector<int> a, std::vector<int> b_t, int n, double h) {
   //
   // Calculating g-tilde
   // Arguments: a, b-tilde, n, h
-  std::vector<double> g_tilde(n);
-  std::double f_1 = 100 * exp( (-10) * h );
+
+  //std::vector<double> g_tilde(n);
+  //std::double f_1 = 100 * exp( (-10) * h );
 
   // Formulars from problem 6.
   for (int i = 1; i < n; i++) {
@@ -57,5 +99,14 @@ std::vector<double> calc_g_t(std::vector<int> a, std::vector<int> b_t, int n, do
     }
   }
 
-  return g_tilde
+  return g_tilde;
+}
+
+std::vector<double> calc_v() {
+  //
+  // Calculating v
+  // Arguments:
+
+
+  return v;
 }
