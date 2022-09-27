64544b56274c2c6e0c2ad7a9550f97b8cd8bded7
Freiamannen <www.fredrik.com@gmail.com>
2022-09-27 13:45:05 +0200

    Made progress on problem 4

---
+++ Projects/project_2/problem_4/header.hpp
@@ -0,0 +1,30 @@
+// Code taken from problem 2 and 3 and built upon.
+
+// Including relevant libraries.
+# include <iostream>
+# include <string>
+# include <vector>
+# include <armadillo>
+# include <cmath>
+# include <cstdlib>
+
+//
+// Forward declaration of relevant functions.
+//
+
+arma::mat tridiagMatGen(int N, double h);
+// Generates/sets up a tridiagonal matrix.
+// Arguments: N, h
+
+double max_offdiag_symmetric(arma::mat A, int& k, int &l);
+// Identifying the largest off-diagonal element of  a matrix
+// Arguments: A, k, l
+// Assuming A is a square and not a single value matrix.
+
+void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l);
+// Preforms a Jacobi rotation at A(k, l) to rotate away the off-diagonal element.
+// Arguments: A, R, k, l
+
+void jacobi_eigensolver(arma::mat& A, arma::mat R, double eps, arma::vec& eigenvalues, arma::mat eigenvectors, const int maxIter, int& iterations, bool& converged);
+// Converging the eigenvalues and eigenvectors using the jacobi_rotate-function.
+// Arguments: A, R, eps, eigenvalues, eigenvectors, maxIter, iterations, converged
---
+++ Projects/project_2/problem_4/main.cpp
@@ -0,0 +1,177 @@
+// Code taken from problem 3, modified and built upon.
+
+// Including the header file.
+# include "header.hpp"
+
+// Defining the main function.
+int main() {
+
+  int N = 6;
+  double n = N + 1;
+  double x_min = 0.0;
+  double x_max = 1.0;
+  double h = (x_max - x_min) / (n);
+  arma::mat R = arma::mat(N, N, arma::fill::eye);
+  std::cout << '\n';
+  R.print();
+  std::cout << '\n';
+  arma::mat A = tridiagMatGen(N, h);
+  double epsilon = 1e-8;
+  arma::vec eigenvalues(N);
+  arma::mat eigenvectors(N, N);
+  int maxIter = 100000;
+  int iterations;
+  bool converged = false;
+
+
+  jacobi_eigensolver(A, R, epsilon, eigenvalues, eigenvectors, maxIter, iterations, converged);
+
+  std::cout << "Eigenvalues: " << '\n';
+  eigenvalues.print(std::cout);
+  std::cout << "Eigenvectors: " << '\n';
+  eigenvectors.print(std::cout);
+
+  return 0;
+}
+
+//
+// Defining the functions
+//
+
+arma::mat tridiagMatGen(int N, double h) {
+  // Generates/sets up the tridiagonal matrix.
+
+  double a = - (1 / (h*h));
+  double d = 2 / (h*h);
+
+  arma::mat A = arma::mat(N, N);
+  A.diag(1).fill(a);
+  A.diag(0).fill(d);
+  A.diag(-1).fill(a);
+
+  return A;
+}
+
+double max_offdiag_symmetric(const arma::mat A, int& k, int &l) {
+  // Identifying the largest off-diagonal element of A matrix
+  // Assuming A is a square, symmetric and not a single value matrix.
+
+  // Get size of the matrix A
+  int rows = A.n_rows;
+
+  // Initialize a double variable maxval to A(k,l)
+  k = 1;
+  l = 0;
+  double maxval = A(k, l);
+
+  // searching through the "upper side" of the matrix, and possibly change the k- and l-values
+  for (int i = 1; i <= rows - 1; i++) {
+    //std::cout << "~~" << '\n';
+    for (int j = 0; j <= i - 1; j++) {
+      //std::cout << "Checking k = " << i << ", l = " << j;
+      //std::cout << " ~~ This makes |A(k, l)| = " << std::abs(A(i, j)) << '\n';
+      if (std::abs(A(i, j)) > maxval) {
+        maxval = std::abs(A(i, j));
+        k = i;
+        l = j;
+      }
+    }
+  }
+
+  return maxval;
+}
+
+void jacobi_rotate(arma::mat& A, arma::mat& R, int k, int l) {
+  // Preforms a Jacobi rotation at A(k, l) to rotate away the off-diagonal element.
+
+  // - Assumes symmetric matrix, so we only consider k < l
+  // - Modifies the input matrices A and R
+
+  // Initialize variables
+  double tau;
+  double tang;
+  double cosi;
+  double sinu;
+
+  double N = A.n_rows;
+
+  // Define the variables
+  tau = ( A(1, 1) -  A(k, k) ) / ( 2 * A(k, l) );
+
+  if (tau > 0) {
+    tang = 1.0 / ( tau + std::sqrt(1 + tau*tau) );
+  }
+  else {
+    tang = 1.0 / ( -tau + std::sqrt(1 + tau*tau) );
+  }
+  cosi = 1.0 / std::sqrt(1 + tang*tang);
+  sinu = cosi * tang;
+
+  // Transforming A-matrix
+  // m:
+  double a_kk_m = A(k, k);
+  double a_ll_m = A(l, l);
+  double a_kl_m = A(k, l);
+  // m+1:
+  A(k, k) = ( a_kk_m * cosi*cosi ) - ( 2 * a_kl_m * cosi * sinu ) + ( a_ll_m * sinu*sinu );
+  A(l, l) = ( a_ll_m * cosi*cosi ) - ( 2 * a_kl_m * cosi * sinu ) + ( a_kk_m * sinu*sinu );
+  A(k, l) = 0;
+  A(l, k) = 0;
+  // Rest of the row and column
+  for (int i = 0; i <= N - 1; i++) {
+    if (i != k && i != 1){
+      // m:
+      double a_ik_m = A(i, k);
+      double a_il_m = A(i, l);
+      // m+1:
+      A(i, k) = ( a_ik_m * cosi ) - ( a_il_m * sinu );
+      A(k, i) = A(i, k);
+      A(i, l) = ( a_il_m * cosi ) - ( a_ik_m * sinu );
+      A(l, i) = A(i, l);
+    }
+
+  // Update the rotation matrix R
+    // m
+    double r_ik_m = R(i, k);
+    double r_il_m = R(i, l);
+    // m+1
+    R(i, k) = ( r_ik_m * cosi ) - ( r_il_m * sinu );
+    R(i, l) = ( r_il_m * cosi ) - ( r_ik_m * sinu );
+  }
+
+}
+
+void jacobi_eigensolver(arma::mat& A, arma::mat R, double eps, arma::vec& eigenvalues, arma::mat eigenvectors, const int maxIter, int& iterations, bool& converged) {
+  // Converging the eigenvalues and eigenvectors using the jacobi_rotate-function.
+
+  // Finding the largest off-diagonal element initially
+  int k = 0;
+  int l = 1;
+  double maxVal = max_offdiag_symmetric(A, k, l);
+
+  // Preforming Jacobi rotations while counting the number of iterations
+  // until the tolerance is met.
+  while(!converged) {
+    jacobi_rotate(A, R, k, l);
+    iterations++;
+
+    maxVal = max_offdiag_symmetric(A, k, l);
+
+    if (maxVal > eps || maxIter > iterations) {
+      converged = true;
+    }
+  }
+
+  std::cout << "\n" << "~~" << "\n" << "No. iterations: " << iterations << "\n" << "~~" << "\n";
+
+  // Gathering data
+  eigenvalues = A.diag();
+  eigenvalues = sort(eigenvalues);
+  arma::uvec indecesSorted = arma::sort_index(eigenvalues);
+  int N = R.n_rows;
+  eigenvectors = R;
+  for (int i = 0; i < N; i++) {
+    eigenvectors.col(i) = R.col( indecesSorted(i) );
+  }
+
+}
